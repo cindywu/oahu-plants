27dad94b9f8293f773d04c8442a775938a0534fa
cindy <1177031+cindywu@users.noreply.github.com>
2023-02-15 15:23:03 -1000

    lettuce begin

---
+++ components/plant.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Plant({rep}) {
+  console.log({rep})
+  return (
+    <div>Plant</div>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    REPLICACHE_LICENSE_KEY:  'lcae3d0a7f40f448883533f62d0a96a76',
+  }
 }
 
 module.exports = nextConfig
